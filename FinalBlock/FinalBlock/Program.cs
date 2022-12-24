e780cee8e5858ed9343542b949017e181c5124ef
Alexandr <hitrin79@gmail.com>
2022-12-24 17:45:02 +0800

    Создание программы

---
+++ FinalBlock/FinalBlock/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] array = AskArray();
+
+void outupArray(string[] array)
+{
+    string[] input = new string[array.Length];
+     for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            input[i] = array[i]; ;
+            Console.Write($"{input[i]}, ");
+        }
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"{Array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+string[] AskArray()
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+outupArray(array);
+Console.WriteLine();
+PrintArray(array);
